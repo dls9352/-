3607264e96d014db9a02b3c38a2cd755f586eb27
dls9352 <129916237+dls9352@users.noreply.github.com>
2023-04-13 00:00:44 +0900

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,22 @@
+function Inputvalue(text){
+    document.getElementById('result').value += text
+}
+function Valueclear(){
+    document.getElementById('result').value = ""
+}
+function Valueslice(){
+    document.getElementById('result').value = document.getElementById('result').value.slice(0,-1)
+}
+function Valueout(){
+    var Final = document.getElementById('result').value;
+    document.getElementById('result').value = eval(
+        Final.replace(/x/g,"*").replace(/÷/g, "/")
+        )
+}
+var math = ["+", "-", "x", "÷",]
+function solo(text){
+    var last = document.getElementById('result').value.slice(-1);
+    if (!math.includes(last)){
+        document.getElementById('result').value += text
+    }
+}
